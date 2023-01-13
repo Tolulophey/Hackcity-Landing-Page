da5a902f1bed46f78c07d2b398d3af28d4a061a8
Tolulope Amole <amoletolulope@gmail.com>
2023-01-13 21:06:25 +0100

    team desktop screen done

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ButtonOne from '../../reusables/ButtonOne/ButtonOne'
 import './team.css'
 
 function Team() {
   return (
     <div className='team'>
-      Team
-
+      <h2>Join our Team</h2>
+      <p className="title">Great Community for Experienced Developers</p>
+      <p>With Hack City Tech., we not only provide startups with the right tech team to build their products efficiently, we also provide marketing and business operations support/resources.</p>
+      <ButtonOne text={"Join us"}/>
     </div>
   )
 }
