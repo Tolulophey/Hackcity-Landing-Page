ef2841353df49879198c22563e8823b2b0f4a0ec
Tolulope Amole <amoletolulope@gmail.com>
2023-01-15 19:04:05 +0100

    business component image splitted

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./business.css"
-import businessImage from '../../assets/successful-business-image.png'
+import startup1 from '../../assets/startup-1.png'
+import startup2 from '../../assets/startup-2.png'
+import startup3 from '../../assets/startup-3.png'
+import startup4 from '../../assets/startup-4.png'
+import startup5 from '../../assets/startup-5.png'
+import startup6 from '../../assets/startup-6.png'
+import startup7 from '../../assets/startup-7.png'
 
 function Business() {
+  const image = [startup1, startup2, startup3, startup4, startup5, startup6, startup7]
   return (
     <div className='business'>
       <h2>Successful  Businesses & Startups</h2>
-      <img src={businessImage} alt="successful-business" />
+      <div className="images">
+        {image.map((image, index)=>{
+          return <img 
+                  src={image} 
+                  alt="successful-business" 
+                  className={`business-image-${index+1}`} 
+                />
+        })}
+      </div>
     </div>
   )
 }
